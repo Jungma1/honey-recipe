53222436af8cc313b1347d109652f1e4f17257e4
Jungma <mbjun3630@naver.com>
2023-01-06 13:45:34 +0900

    feat: 검증 파이프 추가

--- honey-server/src/main.ts
+++ honey-server/src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
