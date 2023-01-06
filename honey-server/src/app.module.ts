8f818c7479e6b8262b7ac5fa98a829b162700a2d
Jungma <mbjun3630@naver.com>
2023-01-06 15:04:02 +0900

    feat: prisma 전역 모듈로 변경

--- honey-server/src/app.module.ts
+++ honey-server/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  providers: [PrismaService],
+  imports: [PrismaModule],
 })
 export class AppModule {}
