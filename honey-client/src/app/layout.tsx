6947bb5b54c1769c86c0ebf6341efbda8bb0bd88
Jungma <mbjun3630@naver.com>
2023-01-19 17:07:52 +0900

    feat: 프론트 셋업

---
+++ honey-client/src/app/layout.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
