8bef1ae631298849e877b0b7725599f4e775efd5
Jungma <mbjun3630@naver.com>
2023-07-05 21:10:41 +0900

    refactor: axios

--- honey-client/src/app/apis/index.ts
+++ honey-client/src/app/apis/index.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-export const client = axios.create({
-  withCredentials: true,
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+const createClient = () => {
+  const _client = axios.create({
+    withCredentials: true,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+  });
+  return _client;
+};
+
+export const client = createClient();
--- honey-client/src/app/apis/user.ts
+++ honey-client/src/app/apis/user.ts
@@ -2,10 +2,6 @@ import { client } from '.';
 import { User } from './types';
 
 export const getProfile = async () => {
-  try {
-    const response = await client.get<User>('/api/v1/auth/profile');
-    return response.data;
-  } catch (error) {
-    return null;
-  }
+  const response = await client.get<User>('/api/v1/auth/profile');
+  return response.data;
 };
