c0aec042f787514ff3ae4721fa18cdcfd521998f
Jungma <mbjun3630@naver.com>
2023-07-18 00:56:46 +0900

    fix: filename conflict

--- honey-client/src/pages/recipe/write.tsx
+++ honey-client/src/pages/recipe/write.tsx
@@ -11,7 +11,7 @@ import { useImageFileSelector } from '~/hooks/useImageFileSelector';
 import { useRecipeStore } from '~/stores/recipe';
 import { validateTokenCookie } from '~/utils/cookie';
 import { json } from '~/utils/json';
-import { redirect } from '~/utils/route';
+import { redirect } from '~/utils/router';
 
 export default function RecipeWritePage() {
   const { form, errorMessage, changeErrorMessage, changeThumbnail } = useRecipeStore();
