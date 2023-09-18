63853c3dd400000c9bf5142f8ccd98bde3b9c56d
Jungma <mbjun3630@naver.com>
2023-09-18 14:54:44 +0900

    fix: user profile image nullable

--- honey-server/src/api/auth/dto/auth-user.dto.ts
+++ honey-server/src/api/auth/dto/auth-user.dto.ts
@@ -4,12 +4,12 @@ export class AuthUserDto {
   id: number;
   handle: string;
   username: string;
-  picture: string;
+  picture: string | null;
 
   constructor(user: User) {
     this.id = user.id;
     this.handle = user.handle;
     this.username = user.username;
-    this.picture = user.picture;
+    this.picture = user.picture ?? null;
   }
 }
